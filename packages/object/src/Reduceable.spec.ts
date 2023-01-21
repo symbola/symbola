fb86787b0854b8a1846028e81ff37a4d94fbe89d
slikts <dabas@untu.ms>
2023-01-21 13:58:43 +0000

    feat(object): add Reduceable protocol

---
+++ packages/object/src/Reduceable.spec.ts
@@ -0,0 +1,21 @@
+import { reduce } from './Reduceable'
+
+describe('Reduceable', () => {
+  it('reduces iterables', () => {
+    const iterable = new Set([1, 2])
+    const result = iterable[reduce]((a, b) => a + b)
+    expect(result).toEqual(3)
+  })
+
+  it('reduces iterables with initial value', () => {
+    const iterable = new Set([1, 2, 3])
+    const result = iterable[reduce]((a, b) => a + b, 1)
+    expect(result).toEqual(7)
+  })
+
+  it('reduces iterables to a different type', () => {
+    const iterable = new Set([1, 2, 3])
+    const result = iterable[reduce]((a, b) => `${a}${b}`, '')
+    expect(result).toEqual('123')
+  })
+})
---
+++ packages/object/src/Reduceable.ts
@@ -0,0 +1,34 @@
+import { extend } from '@symbola/core'
+
+export const reduce = Symbol('reduce')
+
+export default abstract class Reduceable {
+  [reduce]<A>(this: Iterable<A>, fn: (p: A, c: A) => A): A
+  [reduce]<A>(this: Iterable<A>, fn: (p: A, c: A) => A, init: A): A
+  [reduce]<A, B>(this: Iterable<A>, fn: (p: B, c: A) => B, init: B): B
+  [reduce](
+    this: Iterable<unknown>,
+    fn: (...args: unknown[]) => unknown,
+    ...args: unknown[]
+  ): unknown {
+    const iterator = this[Symbol.iterator]()
+    let previous = args.length ? args[0] : iterator.next().value
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const { value, done } = iterator.next()
+      if (done) {
+        break
+      }
+      const next = fn(previous, value)
+      previous = next
+    }
+    return previous
+  }
+}
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Object extends Reduceable {}
+}
+
+extend(Object.prototype, Reduceable.prototype)
