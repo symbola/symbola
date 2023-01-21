7216142fcf78e4d7d7f84f45dbe9b07414c8d3fc
slikts <dabas@untu.ms>
2023-01-21 15:18:48 +0000

    chore(object)!: update exports

--- packages/object/src/index.ts
+++ packages/object/src/index.ts
@@ -1,2 +1,4 @@
-export { map } from '@symbola/core'
-export { default as ExtendedObject } from './ExtendedObject'
+export { default as Traversable, traverse, _break, _continue } from './Traversable'
+export { default as Reducable, reduce } from './Reduceable'
+export { default as Mappable, map } from './Mappable'
+export { default as Filterable, filter } from './Filterable'
