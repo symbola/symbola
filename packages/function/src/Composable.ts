b33a0ea924f6c9c139ae3a134aadb10872471236
slikts <dabas@untu.ms>
2023-01-21 19:18:23 +0000

    docs(function): add alpha tag

--- packages/function/src/Composable.ts
+++ packages/function/src/Composable.ts
@@ -4,6 +4,11 @@ import { FunctionN } from 'fp-ts/function'
 export const compose = Symbol('compose')
 
 export default abstract class Composable {
+  /**
+   * TODO: support more arities
+   *
+   * @alpha
+   */
   [compose]<A, B, C>(this: FunctionN<[A], B>, f: FunctionN<[B], C>): FunctionN<[A], C> {
     return (i: A): C => f(this(i))
   }
