998965ba3cb6d7b3682f12b720bf1eac1cc2335d
slikts <dabas@untu.ms>
2023-01-21 23:55:57 +0000

    feat(set): add set methods

--- packages/set/src/ExtendedSet.spec.ts
+++ packages/set/src/ExtendedSet.spec.ts
@@ -1,3 +1,50 @@
-// import { to } from './ExtendedSet'
+import {
+  intersection,
+  union,
+  difference,
+  symmetricDifference,
+  isSubsetOf,
+  isSupersetOf,
+  isDisjointFrom,
+} from './ExtendedSet'
 
-describe('ExtendedSet', () => {})
+describe('ExtendedSet', () => {
+  it('intersects', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect([...a[intersection](b)]).toEqual([2, 3])
+  })
+  it('unites', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect([...a[union](b)]).toEqual([1, 2, 3, 4])
+  })
+  it('differs', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect([...a[difference](b)]).toEqual([1])
+  })
+  it('symmetrically differs', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect([...a[symmetricDifference](b)]).toEqual([1, 4])
+  })
+  it('checks subset', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect(a[isSubsetOf](b)).toBe(false)
+    expect(b[isSubsetOf](a)).toBe(false)
+  })
+  it('checks superset', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect(a[isSupersetOf](b)).toBe(false)
+    expect(b[isSupersetOf](a)).toBe(false)
+  })
+  it('checks disjoint', () => {
+    const a = new Set([1, 2, 3])
+    const b = new Set([2, 3, 4])
+    expect(a[isDisjointFrom](b)).toBe(false)
+    expect(b[isDisjointFrom](a)).toBe(false)
+  })
+})
--- packages/set/src/ExtendedSet.ts
+++ packages/set/src/ExtendedSet.ts
@@ -1,5 +1,4 @@
 import { extend } from '@symbola/core'
-import { filter } from '@symbola/iterable'
 
 export const intersection = Symbol('intersection')
 export const union = Symbol('union')
@@ -10,11 +9,33 @@ export const isSupersetOf = Symbol('isSupersetOf')
 export const isDisjointFrom = Symbol('isDisjointFrom')
 
 /**
+ * Methods from [Set Methods for JavaScript proposal][proposal].
+ *
+ * [proposal]: https://github.com/tc39/proposal-set-methods
+ *
  * @alpha
  */
 export default abstract class ExtendedSet {
   [intersection]<A>(this: Set<A>, other: Set<A>): Set<A> {
-    // return new this[filter]((a) => other.has(a))
+    return new Set([...this].filter((a) => other.has(a)))
+  }
+  [union]<A>(this: Set<A>, other: Set<A>): Set<A> {
+    return new Set([...this, ...other])
+  }
+  [difference]<A>(this: Set<A>, other: Set<A>): Set<A> {
+    return new Set([...this].filter((a) => !other.has(a)))
+  }
+  [symmetricDifference]<A>(this: Set<A>, other: Set<A>): Set<A> {
+    return new Set([...this[difference](other), ...other[difference](this)])
+  }
+  [isSubsetOf]<A>(this: Set<A>, other: Set<A>): boolean {
+    return [...this].every((a) => other.has(a))
+  }
+  [isSupersetOf]<A>(this: Set<A>, other: Set<A>): boolean {
+    return other[isSubsetOf](this)
+  }
+  [isDisjointFrom]<A>(this: Set<A>, other: Set<A>): boolean {
+    return [...this].every((a) => !other.has(a))
   }
 }
 
