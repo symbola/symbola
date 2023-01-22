67cdca6070816d73172d6f46a93033791fcec400
slikts <dabas@untu.ms>
2023-01-22 01:54:04 +0000

    test(iterable): add coverage

--- packages/iterable/src/Takeable.spec.ts
+++ packages/iterable/src/Takeable.spec.ts
@@ -6,4 +6,22 @@ describe('Takeable', () => {
     const result = iterable[take](3)
     expect([...result]).toEqual([1, 2, 3])
   })
+
+  it.skip('takes from iterables with a negative count', () => {
+    const iterable = new Set([1, 2, 3, 4, 5])
+    const result = iterable[take](-3)
+    expect([...result]).toEqual([3, 4, 5])
+  })
+
+  it('takes from iterables with a count of zero', () => {
+    const iterable = new Set([1, 2, 3, 4, 5])
+    const result = iterable[take](0)
+    expect([...result]).toEqual([])
+  })
+
+  it('takes from iterables with not enough values', () => {
+    const iterable = new Set([1, 2, 3, 4, 5])
+    const result = iterable[take](10)
+    expect([...result]).toEqual([1, 2, 3, 4, 5])
+  })
 })
