332b86200bf996bb4bc08766ee45014572d9d0b7
slikts <dabas@untu.ms>
2023-01-22 10:47:15 +0000

    docs: add SO link

--- packages/object/src/Toable.ts
+++ packages/object/src/Toable.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { extend } from '@symbola/core'
 
 export const to = Symbol('to')
 
 /**
- * // TODO: types break when used with `SetConstructor` because of iterable overloads
+ * TODO: types break when used with `SetConstructor` because of iterable overloads ([SO question][1])
+ *
+ * [1]: https://stackoverflow.com/questions/75196941/how-to-wrap-new-without-losing-the-generic-parameters-for-the-constructed-type
  *
  * @alpha
  */
