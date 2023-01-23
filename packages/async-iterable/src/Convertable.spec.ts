728ebf05c55ca964be83debdefcb5fbc91676753
slikts <dabas@untu.ms>
2023-01-23 20:17:59 +0000

    feat(async): toArray supports sync iterables

--- packages/async-iterable/src/Convertable.spec.ts
+++ packages/async-iterable/src/Convertable.spec.ts
@@ -1,11 +1,27 @@
 import { toArray } from './Convertable'
 
 describe('Convertable', () => {
-  it('collects all values from an async iterable into an array', async () => {
-    const asyncIterable = (async function* () {
-      yield* [1, 2, 3]
-    })()
-    const result = await asyncIterable[toArray]()
-    expect(result).toEqual([1, 2, 3])
+  describe('toArray', () => {
+    it('collects all values from an async iterable into an array', async () => {
+      const asyncIterable = (async function* () {
+        yield* [1, 2, 3]
+      })()
+      const result = await asyncIterable[toArray]()
+      expect(result).toEqual([1, 2, 3])
+    })
+
+    it('collects all values from an array into an array', async () => {
+      const asyncIterable = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]
+      const result = await asyncIterable[toArray]()
+      expect(result).toEqual([1, 2, 3])
+    })
+
+    it('collects all values from an iterable into an array', async () => {
+      const iterable = function* () {
+        yield* [1, 2, 3]
+      }
+      const result = await iterable()[toArray]()
+      expect(result).toEqual([1, 2, 3])
+    })
   })
 })
--- packages/async-iterable/src/Convertable.ts
+++ packages/async-iterable/src/Convertable.ts
@@ -1,4 +1,4 @@
-import { extend } from '@symbola/core'
+import { extend, isIterable } from '@symbola/core'
 
 export const toArray = Symbol('toArray')
 
@@ -9,7 +9,13 @@ export default abstract class Convertable {
   /**
    * Collects all values from an async iterable into an array.
    */
-  async [toArray]<A>(this: AsyncIterable<A>) {
+  async [toArray]<A>(this: AsyncIterable<A> | Iterable<A>) {
+    if (Array.isArray(this)) {
+      return Promise.all(this)
+    }
+    if (isIterable(this)) {
+      return Promise.all([...this])
+    }
     const results = []
     for await (const value of this) {
       results.push(value)
