1afeab005b360f9ff7e2e9e13ffff75acdc5f054
slikts <dabas@untu.ms>
2023-01-24 16:55:49 +0000

    feat(iterable): add skip and take methods

--- packages/iterable/src/Sliceable.spec.ts
+++ packages/iterable/src/Sliceable.spec.ts
@@ -1,4 +1,4 @@
-import { slice } from './Sliceable'
+import { slice, take, skip } from './Sliceable'
 
 describe('Sliceable', () => {
   it('slices', () => {
@@ -9,5 +9,85 @@ describe('Sliceable', () => {
     expect([...array[slice](3)]).toEqual([4, 5])
     expect([...array[slice](1)]).toEqual([2, 3, 4, 5])
     expect([...array[slice](undefined, 3)]).toEqual([1, 2, 3])
+  })
+
+  describe('take', () => {
+    it('takes from the beginning of arrays', () => {
+      const array = [1, 2, 3]
+
+      expect([...[][take](0)]).toEqual([])
+      expect([...[][take](1)]).toEqual([])
+      expect([...array[take](1)]).toEqual([1])
+      expect([...array[take](2)]).toEqual([1, 2])
+      expect([...array[take](3)]).toEqual([1, 2, 3])
+      expect([...array[take](5)]).toEqual([1, 2, 3])
+    })
+
+    it('takes from the end of arrays', () => {
+      const array = [1, 2, 3]
+
+      expect([...[][take](-1)]).toEqual([])
+      expect([...array[take](-1)]).toEqual([3])
+      expect([...array[take](-2)]).toEqual([2, 3])
+      expect([...array[take](-5)]).toEqual([1, 2, 3])
+    })
+
+    it('takes from the beginning of iterables', () => {
+      const iterable = new Set([1, 2, 3])
+
+      expect([...iterable[take](1)]).toEqual([1])
+      expect([...iterable[take](2)]).toEqual([1, 2])
+      expect([...iterable[take](3)]).toEqual([1, 2, 3])
+      expect([...iterable[take](5)]).toEqual([1, 2, 3])
+    })
+
+    it('takes from the end of iterables', () => {
+      const iterable = new Set([1, 2, 3])
+
+      expect([...iterable[take](-1)]).toEqual([3])
+      expect([...iterable[take](-2)]).toEqual([2, 3])
+      expect([...iterable[take](-5)]).toEqual([1, 2, 3])
+    })
+  })
+
+  describe('skip', () => {
+    it('skips from the beginning of arrays', () => {
+      const array = [1, 2, 3]
+
+      expect([...[][skip](0)]).toEqual([])
+      expect([...[][skip](1)]).toEqual([])
+      expect([...array[skip](1)]).toEqual([2, 3])
+      expect([...array[skip](2)]).toEqual([3])
+      expect([...array[skip](3)]).toEqual([])
+      expect([...array[skip](5)]).toEqual([])
+    })
+
+    it('skips from the end of arrays', () => {
+      const array = [1, 2, 3]
+
+      expect([...[][skip](-1)]).toEqual([])
+      expect([...array[skip](-1)]).toEqual([1, 2])
+      expect([...array[skip](-2)]).toEqual([1])
+      expect([...array[skip](-3)]).toEqual([])
+      expect([...array[skip](-5)]).toEqual([])
+    })
+
+    it('skips from the beginning of iterables', () => {
+      const iterable = new Set([1, 2, 3])
+
+      expect([...iterable[skip](1)]).toEqual([2, 3])
+      expect([...iterable[skip](2)]).toEqual([3])
+      expect([...iterable[skip](3)]).toEqual([])
+      expect([...iterable[skip](5)]).toEqual([])
+    })
+
+    it('skips from the end of iterables', () => {
+      const iterable = new Set([1, 2, 3])
+
+      expect([...iterable[skip](-1)]).toEqual([1, 2])
+      expect([...iterable[skip](-2)]).toEqual([1])
+      expect([...iterable[skip](-3)]).toEqual([])
+      expect([...iterable[skip](-5)]).toEqual([])
+    })
   })
 })
--- packages/iterable/src/Sliceable.ts
+++ packages/iterable/src/Sliceable.ts
@@ -1,8 +1,89 @@
 import { extend } from '@symbola/core'
+import { Buffer } from 'queueable'
+
+import { reverse } from './Reversable'
+import { buffer } from './Bufferable'
 
 export const slice = Symbol('slice')
+export const splice = Symbol('slice')
+export const first = Symbol('first')
+export const last = Symbol('last')
+export const take = Symbol('take')
+export const skip = Symbol('skip')
+export const at = Symbol('at')
 
 export default abstract class Sliceable {
+  *[take]<A>(this: Iterable<A>, count: number) {
+    if (count === 0) {
+      return
+    } else if (count === 1) {
+      yield this[first]()
+    } else if (count === -1) {
+      yield this[last]()
+    } else if (Array.isArray(this)) {
+      yield* count > 0 ? this.slice(0, count) : this.slice(count)
+    } else if (count === Infinity) {
+      yield* this
+    } else if (count === -Infinity) {
+      yield* this[reverse]()
+    } else if (count < 0) {
+      yield* this[buffer](Math.abs(count))
+    } else {
+      let i = 0
+      for (const value of this) {
+        if (i === count) {
+          break
+        }
+        yield value
+        i += 1
+      }
+    }
+  }
+
+  *[skip]<A>(this: Iterable<A>, count: number) {
+    if (count === 0) {
+      return
+    } else if (Array.isArray(this)) {
+      yield* count > 0 ? this.slice(count) : this.slice(0, count)
+    } else if (count > 0) {
+      for (const value of this) {
+        if (count === 0) {
+          yield value
+        } else {
+          count -= 1
+        }
+      }
+    } else {
+      const buffer = Buffer.from(this, Infinity)
+      yield* buffer[take](Math.max(0, buffer.length + count))
+    }
+  }
+  /**
+   * Gets the first element of the iterable.
+   */
+  [first]<A>(this: Iterable<A>) {
+    if (Array.isArray(this)) {
+      return this[0]
+    }
+    for (const value of this) {
+      return value
+    }
+  }
+
+  /**
+   * Gets the last element of the iterable.
+   */
+  [last]<A>(this: Iterable<A>) {
+    if (Array.isArray(this)) {
+      return this[this.length - 1]
+    }
+    let lastValue: A | undefined
+    for (const value of this) {
+      lastValue = value
+    }
+    return lastValue
+  }
+
   /**
    * Returns a slice of the iterable.
    */
