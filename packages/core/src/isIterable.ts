d1975eaf37612886afb0df3657d4bed66b57bddc
slikts <dabas@untu.ms>
2023-06-10 17:41:10 +0300

    chore: fix duplicate export

--- packages/core/src/isIterable.ts
+++ packages/core/src/isIterable.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const isIterable = (x: any): x is Iterable<unknown> =>
-  typeof x[Symbol.iterator] === 'function'
+const isIterable = (x: any): x is Iterable<unknown> => typeof x[Symbol.iterator] === 'function'
 
 export default isIterable
